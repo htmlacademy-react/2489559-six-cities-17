e2442c821758895dfd4f7b64df7f1cbe80b573d5
Goryunofff <goryunov.ivan436@yandex.ru>
2025-01-13 14:28:40 +0300

    6.16. Контроль и ограничения (часть 2)

--- src/mocks/offers.ts
+++ src/mocks/offers.ts
@@ -162,4 +162,27 @@ export const mockOffers: Offers[] = [
     rating: 4,
     previewImage: 'img/room.jpg',
   },
+  {
+    id: '5af6f711-c28d-4121-82cd-e0b462a27f00',
+    title: 'Wood and stone place',
+    type: 'Room',
+    price: 100,
+    city: {
+      name: 'Dusseldorf',
+      location: {
+        latitude: 52.35514938496378,
+        longitude: 4.673877537499948,
+        zoom: 8,
+      },
+    },
+    location: {
+      latitude: 52.35514938496378,
+      longitude: 4.673877537499948,
+      zoom: 8,
+    },
+    isFavorite: true,
+    isPremium: false,
+    rating: 2,
+    previewImage: 'img/room.jpg',
+  },
 ];
